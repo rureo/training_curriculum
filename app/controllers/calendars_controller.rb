3adbffb2510ea528ae3fb6a500caa73dea1008ba
rureo <reonakatsuru030623@gmail.com>
2021-05-15 16:43:29 +0900

    issue2の修正　再度

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -12,7 +12,7 @@ class CalendarsController < ApplicationController
     redirect_to action: :index
   end
 
-  
+
   private
 
   def plan_params
@@ -35,7 +35,12 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month : (@todays_date + x).month, :date : (@todays_date+x).day, :plans : today_plans}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+     
+     
+     
+     
+     
       @week_days.push(days)
     end
 
